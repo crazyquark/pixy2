e8f632f0caf1db4db89f66f737806f06aa81b647
crazyquark <cristian.sandu@gmail.com>
2020-03-15 00:27:48 +0200

    fix build script

--- scripts/build_python_demos.sh
+++ scripts/build_python_demos.sh
@@ -26,10 +26,14 @@ TARGET_BUILD_FOLDER=../build
 mkdir $TARGET_BUILD_FOLDER
 mkdir $TARGET_BUILD_FOLDER/python_demos
 
+if [[ -z ${PYTHON} ]]; then
+  PYTHON=python
+fi
+
 cd ../src/host/libpixyusb2_examples/python_demos
 
 swig -c++ -python pixy.i
-python swig.dat build_ext --inplace -D__LINUX__
+${PYTHON} swig.dat build_ext --inplace -D__LINUX__
 
 files=(../../../../build/python_demos/*.so)
 if (( ${#files[@]} )); then
